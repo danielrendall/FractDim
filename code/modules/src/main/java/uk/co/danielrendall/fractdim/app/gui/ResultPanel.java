0645d2e12392d135d6ac7fa0bb93a256cd520cc9
Daniel Rendall <drendall@gmail.com>
2011-02-14 23:23:12 +0000

    Added copyright to files - GPL v3.

--- code/modules/src/main/java/uk/co/danielrendall/fractdim/app/gui/ResultPanel.java
+++ code/modules/src/main/java/uk/co/danielrendall/fractdim/app/gui/ResultPanel.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2009, 2010, 2011 Daniel Rendall
+ * This file is part of FractDim.
+ *
+ * FractDim is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FractDim is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FractDim.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 package uk.co.danielrendall.fractdim.app.gui;
 
 import org.jdesktop.swingx.JXTreeTable;
